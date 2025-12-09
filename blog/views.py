17bb58a32ce9bcbc01f9ef0c285f2487a0747079
JenE87 <engelchen.1408@web.de>
2025-12-09 13:48:29 +0100

    Add FBV pagination to the Blog Post List with Paginator and mobile-first post grid

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,25 @@
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib import messages
 from .models import Post, Idea
 from .forms import IdeaForm
 
 
-def post_list(request, template_name='blog/post_list.html'):
-    posts = Post.objects.filter(published=True).order_by('-published_at')
-    return render(request, template_name, {'posts': posts})
+def post_list(request, *args, **kwargs):
+    """
+    Function-based post list view with pagination (mobile-first grid).
+    Shows 6 posts per page by default.
+    """
+    qs = Post.objects.filter(published=True).order_by('-created_at')
+    paginator = Paginator(qs, 6)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'blog/post_list.html', {
+        'post_list': page_obj.object_list,
+        'page_obj': page_obj,
+        'is_paginated': page_obj.has_other_pages(),
+    })
 
 
 def post_detail(request, slug):
@@ -20,7 +33,7 @@ def idea_submit(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Thanks - your idea has been submitted!")
-            return redirect('home')
+            return redirect('blog:post_list')
     else:
         form = IdeaForm()
     return render(request, 'blog/idea_submit.html', {'form': form})
